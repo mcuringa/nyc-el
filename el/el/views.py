63c507dc56129b9fd2b27dd9a6f4061a7e708e01
katy <katyauchter@gmail.com>
2014-03-20 10:50:15 -0400

    Update views.py
    Added the html file page direction for the 3 new pages. *These html pages do not exist yet, they are just place holders.

--- el/el/views.py
+++ el/el/views.py
@@ -6,3 +6,23 @@ def home(request):
     """The home page"""
         
     return render(request, 'home.html')
+
+def link1(request):
+    """The first linked page"""
+    
+    return render(request, 'link1.html')
+
+def link2(request):
+    """The second linked page"""
+    
+    return render(request, 'link2.html')
+    
+def link3(request):
+    """The third linked page"""
+    
+    return render(request, 'link3.html')
+    
+def link4(request):
+    """The tested linked page"""
+    
+    return render(request, 'home.html')
