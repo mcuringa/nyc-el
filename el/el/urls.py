d50ff89ef6f06af3d310e658b562fdd2669ed20f
katy <katyauchter@gmail.com>
2014-03-20 10:47:00 -0400

    Update urls.py
    Added new "link" names to the url page.

--- el/el/urls.py
+++ el/el/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'el.views.home', ),
+    url(r'^$', 'el.views.link1', ),
+    url(r'^$', 'el.views.link2', ),
+    url(r'^$', 'el.views.link3', ),
 )
