03ef5934f4f11791ec8db6b146d7792afcf53369
katy <katyauchter@gmail.com>
2014-03-25 14:38:16 -0400

    fixes some issues with urls

--- el/el/urls.py
+++ el/el/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'el.views.home', ),
-    url(r'^$', 'el.views.link1', ),
-    url(r'^$', 'el.views.link2', ),
+    url(r'^link1$', 'el.views.link1', ),
+    url(r'^link2$', 'el.views.link2', ),
     url(r'^$', 'el.views.link3', ),
     url(r'^$', 'el.views.link4', ),
     url(r'^$', 'el.views.link5', ),
