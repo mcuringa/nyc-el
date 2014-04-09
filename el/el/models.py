5744e3f2b5e9e0e788c18ce5f46eb8d186cd9a94
katy <katyauchter@users.noreply.github.com>
2014-04-08 20:23:11 -0400

    Create models.py

---
+++ el/el/models.py
@@ -0,0 +1,23 @@
+https://docs.djangoproject.com/en/dev/ref/models/fields/
+
+class City(models.Model):
+  
+  name = models.CharField(max_length = 50)
+  population = models.IntegerField(default = 0)
+  square_miles = models.IntegerField(default = 0)
+  square_miles_track = models.IntegerField(default = 0)
+  avg_annual_riders = models.IntegerField(default = 0)
+  cost_tix = models.IntegerField (default = 0) -----find decimal
+  num_stations = models.IntegerField(default = 0)
+  
+  
+  
+  
+class Stations(models.Model):
+  key_facts = models.CharField(max_length=500)
+  ownership = models.CharField(max_length= 500)
+  percent_grwth = models.IntegerField(default = 0 )
+  yr_built = models.CharField(default = 0)
+    
+  
+
