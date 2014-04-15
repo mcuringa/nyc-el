31ffbe3baddfba642c77f49983b47863926b2186
katyauchter <katyauchter@gmail.com>
2014-04-15 17:53:31 -0400

    trys

--- el/el/views.py
+++ el/el/views.py
@@ -38,9 +38,78 @@ def link2(request):
     
 def link3(request):
     """The third linked page"""
+
+    # userInput = buttonSelection  <---This is what I need to use eventually
+    # This is for the test   V
+
+    # userInput= ""
     
-    return render(request, 'link3.html')
+    # data = {"londonbutton" : ["london- cityname", "population#", "location", "ranking in largest transit systems"], "moscowbutton": ["rio-cityname","pop#2", "loc2", "rank2"]}
+
+    # "radio1" = data[londonbutton]
+    # "radio2" = data[moscowbutton]
+
+    # if userInput == "radio1"
+
+    # if buttonSelection == londonbutton
+    #     data["londonbutton"] = ""
+
     
+    # return render(request, 'home.html', data)
+
+
+    # userInput= ""
+# <a href="http://jquery.com/">jQuery</a>
+#  <script src="jquery.js"></script>
+# <script>
+#     if(document.getElementByID('city_london').checked){
+#         userInput = "radio1"
+
+#     }else if(document.getElementById('city_moscow').checked)
+#         userInput = "radio2"
+# </script>
+
+    #     data[londonbutton]
+
+
+
+# <a href="http://jquery.com/">jQuery</a>
+#  <script src="jquery.js"></script>
+
+#     <script>
+#         function check_value(val){    
+#         var cities = ["london", "moscow"];
+#         var city = cities[val];
+#         var el = document.getElementById("postcard");
+#         if (city){
+#         el.src = 
+#         }
+
+
+
+
+# Can the dictionary information be organized into a class system?
+    # class PostCardData:
+
+    #     def __init__(self):
+    #         self.city = "cityName"
+    #         self.pop = "population"
+    #         self.location = "countryName"
+    #         self.transit = "transitTypes/Rank/ ?"
+
+    # london = PostCardData()
+    #     london.city = "london"
+    #     london.pop = "8.174 million"
+    #     london.location = "England"
+    #     london.transit ="track=402 km"
+
+    # moscow = PostCardData()
+    #     moscow.city = "moscow"
+    #     moscow.pop = "11.5 million"
+    #     moscow.location: "Russia"
+    #     moscow.tranit= "route=325.4 km"
+
+
 def link4(request):
     """The tested linked page"""
     
