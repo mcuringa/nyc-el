78b4177be3d8b9f5e9e0041c034f22a907c7435b
Jeff <jefferytrask@gmail.com>
2014-04-17 20:10:15 -0400

    changes to the code

--- el/el/urls.py
+++ el/el/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'el.views.home', ),
     url(r'^link1$', 'el.views.link1', ),
-    url(r'^link2$', 'el.views.link2', ),
+    url(r'^hongkong$', 'el.views.hongkong', ),
     url(r'^tokyo$', 'el.views.tokyo', ),
     url(r'^cityForm$', 'el.views.cityForm', ),
+    url(r'^mexico$', 'el.views.mexico', )
     url(r'^home$', 'el.views.link5', ),
 
 )
